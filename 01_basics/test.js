0ceb144298b447469bfc2cca992d9b352d32fa91
Ayush Kumar <95538915+Ayusk19@users.noreply.github.com>
2024-06-17 16:52:24 +0000

    container config

---
+++ 01_basics/test.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "ayush@gmail.com"
+var accountPassword = "12345"
+accountcity = "jaipur"
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "23292323"
+accountcity = "Bengaluru"
+
+console.log(accountId);
+console.table
