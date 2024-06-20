e8a4950309d5648c37a9dc2c5eb4bf32ce8a23e3
Ayush Kumar <95538915+Ayusk19@users.noreply.github.com>
2024-06-20 16:46:30 +0000

    datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat code as a newer version
+
+// alert("hello")  we are using node js, not browser
+
+console.log(3 + 3); console.log("Ayush") 
+// code readability should be high
+
+// use mdn or ecma script for latest i.e. mdn is created by mozilla and the ecma is original
+// console.log("hello")  // console.log is a function
+// console.log is a function which takes a string as an argument and prints it on the console
+
+let name = "Ayush"  // is string
+let age = 22 // number => 2 to power 53
+let isLoggedIn = false // => boolean => True / False
+let state = null
+// null => standalone value
+// undefined => variable is declared but not initialized
+// bigint
+// string can be used in "" and ''
+// symbol => Unique
+// all above are primitive datatypes
+// object => collection of key value pairs
+console.log(typeof undefined) // undefined datatype
+console.log(typeof null) // object datatype (bug in js)
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,12 +2,18 @@ const accountId = 144553
 let accountEmail = "ayush@gmail.com"
 var accountPassword = "12345"
 accountcity = "jaipur"
-
+let accountState;
 // accountId = 2
 
 accountEmail = "hc@hc.com"
 accountPassword = "23292323"
 accountcity = "Bengaluru"
 
+
+
+/*
+prefer not to use var 
+beacuse of issue in block scope and functional scope
+*/
 console.log(accountId);
-console.table
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState])
